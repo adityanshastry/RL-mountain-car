6d9f4245914f1154654957e6a841a8f3e4a52c5b
adityanshastry <adityashastry1992@gmail.com>
2017-11-08 12:19:44 -0500

    plotting the rewards and stddev

--- Q_3.py
+++ Q_3.py
@@ -12,7 +12,7 @@ def main(args):
     if args == "1":
         print "Sarsa"
         trial_ranges = Utils.get_trial_splits(total_trials)
-        sarsa_results = Parallel(n_jobs=10)(
+        sarsa_results = Parallel(n_jobs=20)(
             delayed(Trainer.train_sarsa)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                          gamma=gamma, fourier_basis_order=fourier_basis_order,
                                          total_trials=total_trials)
@@ -22,7 +22,7 @@ def main(args):
     elif args == "2":
         print "Q-Learning"
         trial_ranges = Utils.get_trial_splits(total_trials)
-        q_learning_results = Parallel(n_jobs=10)(
+        q_learning_results = Parallel(n_jobs=20)(
             delayed(Trainer.train_q_learning)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                               gamma=gamma, fourier_basis_order=fourier_basis_order,
                                               total_trials=total_trials)
@@ -30,9 +30,18 @@ def main(args):
         np.savetxt("q_learning_rewards.txt", np.sum(q_learning_results, axis=0))
 
     elif args == "3":
-        sarsa_results = np.loadtxt("sarsa_rewards.txt")
-        q_learning_results = np.loadtxt("q_learning_rewards.txt")
-        Trainer.plot_rewards_and_episodes(sarsa_results, q_learning_results)
+        sarsa_results = np.loadtxt("sarsa_rewards_2.txt")
+        q_learning_results = np.loadtxt("q_learning_rewards_2.txt")
+        avg_sarsa = np.average(sarsa_results, axis=0)
+        avg_q_learning = np.average(q_learning_results, axis=0)
+        stddev_sarsa = np.std(sarsa_results, axis=0)
+        stddev_q_learning = np.std(q_learning_results, axis=0)
+        # print avg_sarsa.shape
+        # print avg_q_learning.shape
+        # print stddev_sarsa.shape
+        # print stddev_q_learning.shape
+        Trainer.plot_rewards_and_episodes(avg_sarsa, stddev_sarsa, "SARSA", "o", "b")
+        Trainer.plot_rewards_and_episodes(avg_q_learning, stddev_q_learning, "Q-Learning", "^", "r")
 
 
 if __name__ == '__main__':
