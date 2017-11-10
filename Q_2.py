3e7e0474336e9e56cdbc76c57a84c6aba8cfc593
adityanshastry <adityashastry1992@gmail.com>
2017-11-09 19:56:34 -0500

    adding number of processes as parameter

--- Q_2.py
+++ Q_2.py
@@ -19,8 +19,8 @@ def test():
 
     results = Parallel(n_jobs=10)(
         delayed(parallelize)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
-                                     gamma=gamma, fourier_basis_order=fourier_basis_order,
-                                     total_trials=total_trials)
+                             gamma=gamma, fourier_basis_order=fourier_basis_order,
+                             total_trials=total_trials)
         for lr in lr_range
         for epsilon in epsilon_range
         for gamma in gamma_range
@@ -40,15 +40,16 @@ def test():
 
 
 def main(args):
+
     total_trials, num_episodes = 100, 200
     lr_range = np.linspace(0.01, 0.05, num=5)
     epsilon_range = np.linspace(0.6, 0.9, num=4)
     gamma_range = np.linspace(0.6, 0.9, num=4)
     fourier_basis_order_range = np.linspace(1, 4, num=4)
 
-    if args == "1":
+    if args[0] == "1":
         trial_ranges = Utils.get_trial_splits(total_trials)
-        sarsa_results = Parallel(n_jobs=10)(
+        sarsa_results = Parallel(n_jobs=int(args[1]))(
             delayed(Trainer.train_sarsa)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                          gamma=gamma, fourier_basis_order=fourier_basis_order,
                                          total_trials=total_trials)
@@ -57,11 +58,11 @@ def main(args):
             for gamma in gamma_range
             for fourier_basis_order in fourier_basis_order_range
             for trial_range in trial_ranges)
-        np.savetxt("sarsa_hyperparameters.txt", sarsa_results)
+        np.save("results/sarsa_hyperparameters", sarsa_results)
 
-    elif args == "2":
+    elif args[0] == "2":
         trial_ranges = Utils.get_trial_splits(total_trials)
-        q_learning_results = Parallel(n_jobs=10)(
+        q_learning_results = Parallel(n_jobs=int(args[1]))(
             delayed(Trainer.train_q_learning)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                               gamma=gamma, fourier_basis_order=fourier_basis_order,
                                               total_trials=total_trials)
@@ -70,31 +71,25 @@ def main(args):
             for gamma in gamma_range
             for fourier_basis_order in fourier_basis_order_range
             for trial_range in trial_ranges)
-        np.savetxt("q_learning_hyperparameters.txt", q_learning_results)
+        np.save("results/q_learning_hyperparameters", q_learning_results)
 
-    elif args == "3":
-        sarsa_results = np.loadtxt("sarsa_hyperparameters.txt")
-        q_learning_results = np.loadtxt("q_learning_hyperparameters.txt")
-        best_sarsa = np.argmin(np.sum(sarsa_results, axis=1))
-        best_sarsa_params = np.unravel_index(best_sarsa, (1, 4, 4, 4, 5))
-        best_q_learning = np.argmin(np.sum(q_learning_results, axis=1))
-        best_q_learning_params = np.unravel_index(best_q_learning, (1, 4, 4, 4, 5))
-
-        print "Best sarsa params: " \
-              "\n Fourier Basis : ", fourier_basis_order_range[best_sarsa_params[1]], \
-            "\n Gamma : ", gamma_range[best_sarsa_params[2]], "\n Epsilon : ", epsilon_range[best_sarsa_params[3]], \
-            "\n Learning Rate : ", lr_range[best_sarsa_params[4]]
-
-        print "Best q_learning params: " \
-              "\n Fourier Basis : ", fourier_basis_order_range[best_q_learning_params[1]], \
-            "\n Gamma : ", gamma_range[best_q_learning_params[2]], "\n Epsilon : ", epsilon_range[best_q_learning_params[3]], \
-            "\n Learning Rate : ", lr_range[best_q_learning_params[4]]
-
-        Trainer.plot_rewards_and_episodes(sarsa_results[best_sarsa], q_learning_results[best_q_learning])
+    elif args[0] == "3":
+        sarsa_results = np.sum(np.load("results/sarsa_best_params.npy"), axis=0)
+        q_learning_results = np.sum(np.load("results/q_best_params.npy"), axis=0)
+        avg_sarsa = np.average(sarsa_results, axis=0)
+        avg_q_learning = np.average(q_learning_results, axis=0)
+        stddev_sarsa = np.std(sarsa_results, axis=0)
+        stddev_q_learning = np.std(q_learning_results, axis=0)
+        # print avg_sarsa.shape
+        # print avg_q_learning.shape
+        # print stddev_sarsa.shape
+        # print stddev_q_learning.shape
+        Trainer.plot_rewards_and_episodes(avg_sarsa, stddev_sarsa, "SARSA", "o", "b")
+        Trainer.plot_rewards_and_episodes(avg_q_learning, stddev_q_learning, "Q-Learning", "^", "r")
 
     pass
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1:])
     # test()
--- Q_3.py
+++ Q_3.py
@@ -6,32 +6,33 @@ import sys
 
 
 def main(args):
+
     total_trials, num_episodes = 10000, 200
     lr, epsilon, gamma, fourier_basis_order = 0.05, 0.5, 1.0, 1
 
-    if args == "1":
+    if args[0] == "1":
         print "Sarsa"
         trial_ranges = Utils.get_trial_splits(total_trials)
-        sarsa_results = Parallel(n_jobs=20)(
+        sarsa_results = Parallel(n_jobs=int(args[1]))(
             delayed(Trainer.train_sarsa)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                          gamma=gamma, fourier_basis_order=fourier_basis_order,
                                          total_trials=total_trials)
             for trial_range in trial_ranges)
-        np.savetxt("sarsa_rewards.txt", np.sum(sarsa_results, axis=0))
+        np.savetxt("results/sarsa_rewards.txt", np.sum(sarsa_results, axis=0))
 
-    elif args == "2":
+    elif args[0] == "2":
         print "Q-Learning"
         trial_ranges = Utils.get_trial_splits(total_trials)
-        q_learning_results = Parallel(n_jobs=20)(
+        q_learning_results = Parallel(n_jobs=int(args[1]))(
             delayed(Trainer.train_q_learning)(num_trials=trial_range, num_episodes=num_episodes, lr=lr, epsilon=epsilon,
                                               gamma=gamma, fourier_basis_order=fourier_basis_order,
                                               total_trials=total_trials)
             for trial_range in trial_ranges)
-        np.savetxt("q_learning_rewards.txt", np.sum(q_learning_results, axis=0))
+        np.savetxt("results/q_learning_rewards.txt", np.sum(q_learning_results, axis=0))
 
-    elif args == "3":
-        sarsa_results = np.loadtxt("sarsa_rewards_2.txt")
-        q_learning_results = np.loadtxt("q_learning_rewards_2.txt")
+    elif args[0] == "3":
+        sarsa_results = np.loadtxt("results/sarsa_rewards.txt")
+        q_learning_results = np.loadtxt("results/q_learning_rewards.txt")
         avg_sarsa = np.average(sarsa_results, axis=0)
         avg_q_learning = np.average(q_learning_results, axis=0)
         stddev_sarsa = np.std(sarsa_results, axis=0)
@@ -45,4 +46,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1:])
